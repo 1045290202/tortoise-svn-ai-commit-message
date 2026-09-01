805d0f4e2b9763b6115ee9b393b15eda2e200006
来一斤BUG <1045290202@qq.com>
2026-09-01 16:42:54 +0800

    chore: drop pause from wrappers so they close automatically

--- register.cmd
+++ register.cmd
@@ -2,4 +2,3 @@
 rem Register the plugin (current user, no admin) using the Rider Debug build output.
 rem pwsh 7: defaults to UTF-8, avoids the GBK misdecoding of Chinese output.
 "%ProgramFiles%\PowerShell\7\pwsh.exe" -NoProfile -ExecutionPolicy Bypass -File "%~dp0scripts\register.ps1" -DllPath "%~dp0bin\Debug\TsvnAiCommitMessage.dll"
-pause
--- unregister.cmd
+++ unregister.cmd
@@ -2,4 +2,3 @@
 rem Unregister the plugin (current user). The Debug DLL is only read to obtain the CLSID.
 rem pwsh 7: defaults to UTF-8, avoids the GBK misdecoding of Chinese output.
 "%ProgramFiles%\PowerShell\7\pwsh.exe" -NoProfile -ExecutionPolicy Bypass -File "%~dp0scripts\register.ps1" -DllPath "%~dp0bin\Debug\TsvnAiCommitMessage.dll" -Action Unregister
-pause
